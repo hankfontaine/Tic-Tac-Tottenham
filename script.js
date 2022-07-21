81ae85b902c2ed78901ec983cc00d66a48caaa69
HankFontaine <henrymcgill@gmail.com>
2022-07-21 14:54:13 -0400

    worked on style

--- script.js
+++ script.js
@@ -3,10 +3,29 @@ let mainContainer = document.getElementById("container");
 for (i = 1; i <= 9; i++) {
   let cell = document.createElement("div");
   cell.classList.add("cellStyle");
-  //   cell.innerHTML = "X";
   cell.id = "cell" + i;
   mainContainer.appendChild(cell);
 }
+
+let gameSpaceOne = document.getElementById("cell1");
+let gameSpaceTwo = document.getElementById("cell2");
+let gameSpaceThree = document.getElementById("cell3");
+let gameSpaceFour = document.getElementById("cell4");
+let gameSpaceFive = document.getElementById("cell5");
+let gameSpaceSix = document.getElementById("cell6");
+let gameSpaceSeven = document.getElementById("cell7");
+let gameSpaceEight = document.getElementById("cell8");
+let gameSpaceNine = document.getElementById("cell9");
+
+gameSpaceOne.innerHTML = "T";
+gameSpaceTwo.innerHTML = "I";
+gameSpaceThree.innerHTML = "C";
+gameSpaceFour.innerHTML = "T";
+gameSpaceFive.innerHTML = "A";
+gameSpaceSix.innerHTML = "C";
+gameSpaceSeven.innerHTML = "T";
+gameSpaceEight.innerHTML = "Ø";
+gameSpaceNine.innerHTML = "E";
 
 ////////////////// GAME LOGIC ///////////////////////////
 const gameBoard = (() => {
@@ -20,12 +39,12 @@ const gameBoard = (() => {
   };
 })();
 
-gameBoard.addO(cell1);
-gameBoard.addO(cell2);
-gameBoard.addX(cell3);
-gameBoard.addO(cell4);
-gameBoard.addO(cell5);
-gameBoard.addX(cell6);
-gameBoard.addO(cell7);
-gameBoard.addX(cell8);
-gameBoard.addX(cell9);
+// gameBoard.addO(gameSpaceOne);
+// gameBoard.addO(gameSpaceTwo);
+// gameBoard.addX(gameSpaceThree);
+// gameBoard.addO(gameSpaceFour);
+// gameBoard.addO(gameSpaceFive);
+// gameBoard.addX(gameSpaceSix);
+// gameBoard.addO(gameSpaceSeven);
+// gameBoard.addX(gameSpaceEight);
+// gameBoard.addX(gameSpaceNine);
